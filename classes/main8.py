b62f37ce22bf02e9438afbfddb9aaf5a2b042bb1
dilnozaabdijamalova <dilnozaabdijamalova@gmail.com>
2024-12-03 13:47:55 +0500

    Create main8.py

---
+++ classes/main8.py
@@ -0,0 +1,167 @@
+
+
+
+
+#3 
+
+str1 =  "Abdulloh"
+
+
+
+# def my_len(name):
+#     cnt = 0
+#     for i in name:
+#         cnt += 1
+#     print(cnt)
+    
+# str1 =  "Abdulloh"
+# my_len(str1)
+
+
+# def add_itmes(sample_list:list):
+#     res = 0
+#     for i in sample_list:
+#         res +=i
+#     print(res)
+    
+# add_itmes([1,2,3])
+
+
+# def find_max(sampl_list:list):
+#     str = sorted(sampl_list)
+#     print(str[-1])
+   
+ 
+# def multiply_itmes(sample_list:list):
+#     res = 1
+#     for i in sample_list:
+#         res *=i
+#     print(res)
+    
+# multiply_itmes([1,2,3])
+
+
+# def distinct_num(sample_list:list):
+#     new_list =[]
+#     res= [new_list.append(i) for i in sample_list if i not in new_list]
+#     print(res)
+
+
+
+
+
+
+# def is_prime(number:int):
+#     start = 2
+#     while start < number:
+#         if number % start == 0:
+#             print('This is not prime number')
+#             break
+#         else:
+#             start += 1
+#     else:print('This is prime numeber')
+
+# is_prime(22)
+
+
+
+# def find_sth(number:int):
+#     n = 1
+#     while 2**n < number:
+#         print(2**n)
+#         n += 1
+        
+# find_sth(40)
+
+
+
+
+##### Function
+
+# position argument
+# keyword argument
+
+
+# def greetings(name,age):
+#     print(f'My name is {name} and i am {age} years old ')
+    
+# greetings(age = 24,name= 'Husan')
+
+
+numbers = [4,5,6,7,8]
+
+
+#*args
+
+
+# def add_numbers(*args):
+#     res = sum(args)
+#     print(args)
+#     print(res)
+    
+# add_numbers(5,5,8,89,87)
+
+# def classmates(**kvargs):
+#     print(kvargs)
+
+# classmates(name ='Husan',age = 45)
+
+
+# a = 6
+# def func1():
+#     global b
+#     b = 9
+    
+   
+# func1()
+
+   
+# print(a)
+# print(b)
+
+
+# def add_numbers(*args):
+#     res = [i**2 for i in args ]
+#     print(args)
+#     print(res)
+    
+# add_numbers(7,5,8,9)
+
+
+# 0! -- 1
+# 1! -- 1
+# 2! -- 1 * 2 = 2
+# 3! -- 1 * 2 * 3 = 3 * 2!
+# 4! -- 1 * 2 * 3 * 4 = 3! * 4
+
+
+# 5 === 5!
+# 5 === 4! * 5
+# 6! = 5! * 6
+# 7! = 6! * 7
+# 8! = 7! * 8
+
+
+# def factorial(number):
+#     if number in [0,1]:
+#         return 1
+#     else:
+#         return number * factorial(number - 1)
+        
+# print(factorial(10))
+
+
+#   10 * f(9) = 10 * 9 * f(8) = 10 * 9 * 8 * f(7)
+
+
+while 1 < 10:
+    print('ygfuwehfjkweli')
+
+
+ 
+    
+
+
+
+    
+
